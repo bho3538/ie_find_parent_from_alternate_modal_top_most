9e8e467b823125cb15cab4ae4afc0c3918308f07
김병호 <bho3538@inzent.com>
2021-01-22 15:25:54 +0900

    init

---
+++ iealternativetopmost.c
@@ -0,0 +1,25 @@
+﻿
+#include "pch.h"
+#include <stdio.h>
+
+void FindOriginalParentFromFakeModalDialog(HWND dialogHwnd) {
+	WCHAR windowName[MAX_PATH];
+	HWND originalParent = 0;
+
+	GetClassNameW(dialogHwnd, windowName, MAX_PATH);
+	wprintf(L"current window className is %ls \n", windowName);
+
+	//undocumented value (from ieframe.dll)
+	originalParent = GetPropW(dialogHwnd, L"FakeModalPartnerFrame");
+
+	GetWindowTextW(originalParent, windowName, MAX_PATH);
+	wprintf(L"parent window text is %ls , originalhwnd is %x\n", windowName, (DWORD)originalParent);
+}
+
+int main(int argc,char** argv)
+{
+	HWND hwnd = 0;
+	scanf("%x",&hwnd); //('Alternate Modal Top Most' hwnd)
+	FindOriginalParentFromFakeModalDialog(hwnd);
+	return 0;
+}
---
+++ readme.txt
@@ -0,0 +1,5 @@
+Find parent window from 'fake dialog' (Alternate Modal Top Most) in Internet explorer.
+
+see https://lfconfig.wordpress.com/2021/01/21/ie-find-real-parent-window-from-fake-modal-dialog-in-internet-explorer/
+
+License : MIT
